cafa69dea6794c20da949dd6a95df7e8793f9030
gadisa kasaye <gadisaka04@gmail.com>
2025-09-14 09:05:08 +0300

    fix: Correct HTML doctype and title in index.html; remove unused vite.svg; clean up Sidebar component imports

--- src/Store/index.js
+++ src/Store/index.js
@@ -4,7 +4,4 @@ export { default as useGameStore } from "./gameStore";
 export { default as useTransactionStore } from "./transactionStore";
 export { default as useGameSettingsStore } from "./gameSettingsStore";
 export { default as useBotSettingsStore } from "./botSettingsStore";
-<<<<<<< HEAD
 export { default as useBankStore } from "./bankStore";
-=======
->>>>>>> a16113b1b346b738d99766f44cc34ca241bf659f
